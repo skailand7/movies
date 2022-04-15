19115fa05cc84504535b48d45f9b731a6531652a
Miguel Turpo <miganguelturpo@gmail.com>
2022-04-15 18:38:55 -0500

    [FIX] image

--- components/NavList.js
+++ components/NavList.js
@@ -24,6 +24,7 @@ const NavList = ({ setSelection }) => {
             title={item.title}
             option={item.option}
             setSelection={setSelection}
+            key={item.title}
           />
         );
       })}
