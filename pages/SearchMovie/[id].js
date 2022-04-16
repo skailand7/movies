89e63f0a7afca68242bb9ab92959cbec5adf5d6c
Miguel Turpo <miganguelturpo@gmail.com>
2022-04-15 19:17:50 -0500

    [FIX] selections

--- pages/SearchMovie/[id].js
+++ pages/SearchMovie/[id].js
@@ -35,36 +35,39 @@ const Results = () => {
 
   if (!loading) {
     return (
-      <section className="w-full h-screen overflow-y-auto bg-black text-white">
-        <SearchBar />
-        <div className="px-4 py-2 flex space-x-4 items-center">
-          <Link href="/" passHref>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7 16l-4-4m0 0l4-4m-4 4h18"
-              />
-            </svg>
-          </Link>
-          <p className="font-semibold text-xl">
-            Search results for <span className="italic text-sky-600">{id}</span>{" "}
-            ({searchedArr.length})
-          </p>
-        </div>
-        <div className="space-y-8 p-4">
-          {searchedArr.map((item) => {
-            return <Item item={item} key={item.id} />;
-          })}
-        </div>
-      </section>
+      <div className="flex flex-col bg-gray-900 justify-center items-center">
+        <section className="w-full h-screen overflow-y-auto bg-black text-white">
+          <SearchBar />
+          <div className="px-4 py-2 flex space-x-4 items-center">
+            <Link href="/" passHref>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                />
+              </svg>
+            </Link>
+            <p className="font-semibold text-xl">
+              Search results for{" "}
+              <span className="italic text-sky-600">{id}</span> (
+              {searchedArr.length})
+            </p>
+          </div>
+          <div className="space-y-8 p-4">
+            {searchedArr.map((item) => {
+              return <Item item={item} key={item.id} />;
+            })}
+          </div>
+        </section>
+      </div>
     );
   } else {
     return <SkeletonSearch />;
--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -48,30 +48,48 @@ const Movie = () => {
     actorList.length = 10;
 
     return (
-      <div className="bg-gray-900">
-        <div>
-          <div className="relative">
-            <div className="h-[620px] bg-gray-900">
-              <Image
-                layout="fill"
-                objectFit="cover"
-                alt="img"
-                src={arr[0].image}
-              />
-              <div className="hidden md:block absolute right-16 bottom-0 z-30">
+      <div className="flex flex-col bg-gray-900 justify-center items-center">
+        <React.Fragment>
+          <div>
+            <div className="relative">
+              <div className="h-[620px] bg-gray-900">
                 <Image
-                  width="200px"
-                  height="300px"
+                  layout="fill"
+                  objectFit="cover"
                   alt="img"
                   src={arr[0].image}
-                  className="rounded-xl"
                 />
-              </div>
-              <div className="w-8 h-8 flex items-center justify-center fixed top-4 left-4 bg-gray-500/50 rounded-full z-10">
-                <Link href="/" passHref>
+                <div className="hidden md:block absolute right-16 bottom-0 z-30">
+                  <Image
+                    width="200px"
+                    height="300px"
+                    alt="img"
+                    src={arr[0].image}
+                    className="rounded-xl"
+                  />
+                </div>
+                <div className="w-8 h-8 flex items-center justify-center fixed top-4 left-4 bg-gray-500/50 rounded-full z-10">
+                  <Link href="/" passHref>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 text-gray-400 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 19l-7-7 7-7"
+                      />
+                    </svg>
+                  </Link>
+                </div>
+                <div className="w-8 h-8 flex items-center justify-center fixed top-4 right-4 bg-gray-500/50 rounded-full z-10">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-gray-400 "
+                    className="h-5 w-5 text-gray-400"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -80,15 +98,27 @@ const Movie = () => {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M15 19l-7-7 7-7"
+                      d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
                     />
                   </svg>
-                </Link>
+                </div>
               </div>
-              <div className="w-8 h-8 flex items-center justify-center fixed top-4 right-4 bg-gray-500/50 rounded-full z-10">
+              <div className="p-8 w-full h-screen absolute bottom-0 bg-gradient-to-t from-gray-900">
+                <p className="absolute bottom-32 font-bold text-white text-2xl">
+                  {arr[0].title}
+                </p>
+                <div className="flex space-x-4 absolute bottom-24 z-30 font-bold text-white">
+                  {genresArr.map((item) => {
+                    return <Genres item={item} key={item} />;
+                  })}
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center mt-0 mb-6 font-semibold text-white space-x-4">
+              <div className="flex items-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-400"
+                  className="h-5 w-5"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -97,117 +127,89 @@ const Movie = () => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <p className="">{arr[0].runtimeStr}</p>
+              </div>
+              <p>|</p>
+              <div className="flex items-center space-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                </svg>
+                <p>{arr[0].contentRating}</p>
+              </div>
+              <p>|</p>
+              <div className="flex items-center space-x-1">
+                <p>{arr[0].imDbRating}</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
                   />
                 </svg>
               </div>
             </div>
-            <div className="p-8 w-full h-screen absolute bottom-0 bg-gradient-to-t from-gray-900">
-              <p className="absolute bottom-32 font-bold text-white text-2xl">
-                {arr[0].title}
-              </p>
-              <div className="flex space-x-4 absolute bottom-24 z-30 font-bold text-white">
-                {genresArr.map((item) => {
-                  return <Genres item={item} key={item} />;
-                })}
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center mt-0 mb-6 font-semibold text-white space-x-4">
-            <div className="flex items-center space-x-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <p className="">{arr[0].runtimeStr}</p>
-            </div>
-            <p>|</p>
-            <div className="flex items-center space-x-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              </svg>
-              <p>{arr[0].contentRating}</p>
-            </div>
-            <p>|</p>
-            <div className="flex items-center space-x-1">
-              <p>{arr[0].imDbRating}</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-yellow-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-              </svg>
-            </div>
-          </div>
-          <div className="px-4 bg-gray-900 space-x-4 w-auto flex overflow-x-auto overscroll-none text-gray-300">
-            {actorList.map((item) => {
-              return <Cast item={item} key={item.id} />;
-            })}
-          </div>
-          <div className="mt-8 w-full h-auto px-8 -mt-[1px] text-white space-y-4">
-            <p className="mt-8 font-bold  text-xl">Synopsis</p>
-            <article>{arr[0].plot}</article>
-
-            <p
-              className="mt-4 font-semibold text-lg text-cyan-500"
-              onClick={() => setSee(true)}
-            >
-              See Trailer
-            </p>
-            {see ? (
-              <div className="w-full h-auto" id="see-trailer">
-                <iframe
-                  src={`https://www.youtube.com/embed/${trailer}`}
-                  className="w-full h-64"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            ) : null}
-          </div>
-          <div className="px-8 space-y-4 mb-16 mt-8">
-            <p className="text-white font-bold text-2xl">Recommend</p>
-            <section className=" space-x-8 flex w-auto overflow-x-auto ">
-              {similars.map((item) => {
-                return <Similars item={item} key={item.id} />;
+            <div className="px-4 bg-gray-900 space-x-4 w-auto flex overflow-x-auto overscroll-none text-gray-300">
+              {actorList.map((item) => {
+                return <Cast item={item} key={item.id} />;
               })}
-            </section>
+            </div>
+            <div className="mt-8 w-full h-auto px-8 -mt-[1px] text-white space-y-4">
+              <p className="mt-8 font-bold  text-xl">Synopsis</p>
+              <article>{arr[0].plot}</article>
+
+              <p
+                className="mt-4 font-semibold text-lg text-cyan-500"
+                onClick={() => setSee(true)}
+              >
+                See Trailer
+              </p>
+              {see ? (
+                <div className="w-full h-auto" id="see-trailer">
+                  <iframe
+                    src={`https://www.youtube.com/embed/${trailer}`}
+                    className="w-full h-64"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              ) : null}
+            </div>
+            <div className="px-8 space-y-4 mb-16 mt-8">
+              <p className="text-white font-bold text-2xl">Recommend</p>
+              <section className=" space-x-8 flex w-auto overflow-x-auto ">
+                {similars.map((item) => {
+                  return <Similars item={item} key={item.id} />;
+                })}
+              </section>
+            </div>
+            <div className="">
+              <p className="text-center text-white">{arr[0].companies}</p>
+            </div>
           </div>
-          <div className="">
-            <p className="text-center text-white">{arr[0].companies}</p>
-          </div>
-        </div>
+        </React.Fragment>
       </div>
     );
   } else {
