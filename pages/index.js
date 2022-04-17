666996f0612904014539181c537eee892c3bd5e2
Miguel Turpo <miganguelturpo@gmail.com>
2022-04-17 11:25:33 -0500

    [ADD] feature tag

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ const Index = () => {
         <title>MikMovies</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700&display=swap"
           rel="stylesheet"
@@ -71,7 +71,7 @@ const Index = () => {
         </div>
         <style>
           @import
-          url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700&display=swap');
+          url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700&display=swap");
         </style>
       </div>
     </>
